2113622e80f19513c1a6857e2291c8bbe437b6dd
moocow <moo@cow.com>
2026-01-17 01:49:01 +0000

    edited init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
+mkdir ./nginx/certs/
+mkdir ./keys/
+
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./nginx/certs/nginx.key -out ./nginx/certs/nginx.crt -subj "/C=US/ST=State/L=City/O=Organization/OU=Unit/CN=selfsigned.com"
 
 ssh-keygen -t rsa -b 4096 -f ./keys/shellinaboxKEY -N ""
